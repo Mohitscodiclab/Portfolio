74e3d168b431f1715dc1909d433b73fdf1b58ecb
Mohit Kumar <mohitkumar753119@gmail.com>
2025-06-14 00:59:31 +0530

    Update script.js

--- assets/f_d_w/script.js
+++ assets/f_d_w/script.js
@@ -15,4 +15,38 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
-  
+
+
+// new feature added by mohit
+
+const words = ["Fast", "Tasty", "Affordable", "Fresh", "On-Time"];
+let i = 0, j = 0, currentWord = '', isDeleting = false;
+
+function typeEffect() {
+  const typeText = document.getElementById("type");
+
+  if (!typeText) return;
+
+  if (i < words.length) {
+    if (!isDeleting && j <= words[i].length) {
+      currentWord = words[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      currentWord = words[i].substring(0, j--);
+    }
+
+    typeText.innerHTML = currentWord;
+
+    if (j === words[i].length) {
+      isDeleting = true;
+      setTimeout(typeEffect, 1000);
+    } else if (isDeleting && j === 0) {
+      isDeleting = false;
+      i = (i + 1) % words.length;
+      setTimeout(typeEffect, 300);
+    } else {
+      setTimeout(typeEffect, isDeleting ? 50 : 150);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
