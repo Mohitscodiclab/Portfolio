c0d0091d49afdf875a5c4ce02d6cfe2c7e8f1e91
Mohit Kumar <mohitkumar753119@gmail.com>
2025-06-14 01:01:39 +0530

    Update script.js

--- assets/f_d_w/script.js
+++ assets/f_d_w/script.js
@@ -1,22 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const ctaBtn = document.querySelector('.cta-btn');
-    const orderBtns = document.querySelectorAll('.order-btn');
-  
-    // Smooth scroll to the menu section
-    ctaBtn.addEventListener('click', () => {
-      document.getElementById('menu').scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  
-    orderBtns.forEach((btn) => {
-      btn.addEventListener('click', () => {
-        alert('Thank you for ordering! We will deliver your food soon.');
-      });
-    });
-  });
-
-
 // new feature added by mohit
 
 const words = ["Fast", "Tasty", "Affordable", "Fresh", "On-Time"];
@@ -50,3 +31,25 @@ function typeEffect() {
 }
 
 document.addEventListener("DOMContentLoaded", typeEffect);
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const ctaBtn = document.querySelector('.cta-btn');
+    const orderBtns = document.querySelectorAll('.order-btn');
+  
+    // Smooth scroll to the menu section
+    ctaBtn.addEventListener('click', () => {
+      document.getElementById('menu').scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  
+    orderBtns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        alert('Thank you for ordering! We will deliver your food soon.');
+      });
+    });
+  });
+
+
